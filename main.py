ec7b710ed8c3b4a99603335a18f6d1161526fe60
ohyuchan123 <83404310+ohyuchan123@users.noreply.github.com>
2021-07-06 17:41:15 +0900

    python 공부(문자열 기본)

--- main.py
+++ main.py
@@ -1,24 +1,16 @@
-#클래스 생성자
-#평균클래스
-#평균을 내는 기능
-
-#총점 클래스
-#총점을 저장하는 변수
-#총점을 계산하는 기능
-
-class Avg:
-    def __init__(self):
-        self.avg=0
-    def average(self,total):
-        self.avg=self.total/3
-        print("평균<",self.avg)
-class Total(Avg):
-    def total(self,kor,math,eng):
-        self.kor=kor
-        self.math=math
-        self.eng=eng
-        self.total=kor+math+eng
-        self.average(self.total)
-
-person1=Total()
-print(person1.total(92,92,92))
+#문자열 기본
+print(type(32))
+print(type("Hello WOrld...!"))
+print('안녕하세요')
+print("안녕하세요")
+print('"안녕하세요"라고 말했습니다')
+print("'안녕하세요'라고 말했습니다")
+print("\"안녕하세요\"라고 말해습니다")
+print("안녕하세요\n안녕하세요")
+print("안녕하세요\t안녕하세요")
+print("\\")
+print("""안녕하세요
+안녕하세요
+안녕하세요""")
+print()
+print(3*"안녕하세요\n")
