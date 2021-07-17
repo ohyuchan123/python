35ee847520d253ced58a2945097bff3113b6821d
ohyuchan123 <83404310+ohyuchan123@users.noreply.github.com>
2021-07-17 12:49:59 +0900

    python 공부

---
+++ Untitled.py
@@ -0,0 +1,220 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+1+2
+
+
+# In[13]:
+
+
+print('Hello World!!')
+
+
+# In[5]:
+
+
+print("안녕하세요")
+
+
+# In[7]:
+
+
+print('안녕하세요')
+
+
+# In[8]:
+
+
+print(1+2)
+
+
+# In[9]:
+
+
+print(4+5)
+
+
+# In[10]:
+
+
+print("Hello Python")
+
+
+# In[11]:
+
+
+print('hello 임우석 !!')
+print('좋은 Partner가 되어봅시다!!')
+
+
+# In[15]:
+
+
+a=10
+a
+
+
+# In[16]:
+
+
+b=3.14
+b
+
+
+# In[20]:
+
+
+a,b=10,15
+print(a,b)
+
+
+# In[22]:
+
+
+s1=s2="Python"
+print(s1,s2)
+
+
+# In[29]:
+
+
+x=100
+y=200
+sum=x+y
+print(sum)
+print('x:',x,'y:',y,'sum:',sum)
+
+
+# In[32]:
+
+
+multiline='Life is too shrot\nYou nee Python'
+print(multiline)
+print(multilne[3])
+
+
+# In[34]:
+
+
+s="LIfe is too short, Yot need Python"
+print(s[0])
+print(s[12])
+print(s[-1])
+
+
+# In[35]:
+
+
+s="Life is too short, You need Python"
+print(s[0:4])
+
+
+# In[47]:
+
+
+s="Life is too short, You need Python"
+print(s[12:17])
+
+
+# In[58]:
+
+
+s="Life is too short, You need Python"
+print(s[12:])
+
+
+# In[61]:
+
+
+s="Life is too short, You need Python"
+print(s[19:])
+print(s[18:])
+
+
+# In[66]:
+
+
+s="Life is too short, You need Python"
+print(s[:19])
+print(s[:35])
+
+
+# In[67]:
+
+
+s="Life is too short, You need Python"
+print(s[:])
+
+
+# In[106]:
+
+
+s="20210717Rainy"
+year=s[:4]
+month=s[4:8]
+wether=s[8:16]
+print('year :',year)
+print('month :',month)
+print('wether :',wether)
+
+
+# In[107]:
+
+
+print('year :',year,'\nmonth :',month,'\nwether :',wether)
+
+
+# In[116]:
+
+
+s="현재 온도는 18도 입니다"
+s="현재 온도는 20도 입니다"
+
+
+# In[117]:
+
+
+s="현재 온도는 %d도 입니다"%20
+print(s)
+
+
+# In[120]:
+
+
+temp=30
+s="현재 온도는 %d도 입니다"%temp
+print(s)
+
+
+# In[128]:
+
+
+temp=18
+s="광주소프트웨어마이스터고등학교 %d학년 %d학기 종강"%(1,1)
+print(s)
+
+
+# In[130]:
+
+
+s="GSM %d학년 %d학기"%(1,1)
+print(s)
+
+
+# In[137]:
+
+
+x,y=100,200
+sum=x+y
+s="%d과 %d의 합은 %d입니다"%(x,y,sum)
+print(s)
+
+
+# In[ ]:
+
+
+
+
