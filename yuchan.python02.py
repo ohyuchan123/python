8a9e0c041c1211eff86ce9e77cd9a1dc4f11346d
ohyuchan123 <83404310+ohyuchan123@users.noreply.github.com>
2021-07-22 23:29:25 +0900

    Python(자료형-사용자 입력)

--- yuchan.python02.py
+++ yuchan.python02.py
@@ -1,23 +1,14 @@
-#클래스-생성자
-#교수,어린이,학생
-#->사람->먹고 놀고 자고 기능
-#기본클래스(부모클래스)
-class Person:
-    def __init__(self,name,age,job):
-        self.name=name
-        self.age=age
-        self.job=job
-    def eat(self):
-        print(self.name+"님이 먹는다/.")
-    def sleep(self):
-        print(self.name+"님이 잔다/.")
-    def play(self):
-        print(self.name+"님이 논다/.")
-    def __del__(self):
-        print("객체가 소멸합니다/.")
-class Child(Person):
-    pass
-class Student(Person):
-    pass
-class professor(Person):
-    pass
+#자료형
+#사용자 입력 : input()
+string=input("인사말을 입력하세요>")
+print(string)
+#input()함수의 입력 자료형
+print(type(string))
+number=input("숫자를 입력하세요>")
+print(number)
+#input()함수의 입력 자료형
+print(type(number))
+#입력 자료형 확인하기
+string=input("입력>")
+print("자료 : ",string)
+print("자료형 : ",type(string))
