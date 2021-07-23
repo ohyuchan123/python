9bfde20dda826eee5c6cc93558b4db155597d1e7
ohyuchan123 <83404310+ohyuchan123@users.noreply.github.com>
2021-07-23 23:14:50 +0900

    Python(자료형-문자열 format()함수)

--- yuchan.python01.py
+++ yuchan.python01.py
@@ -1,21 +1,41 @@
 #자료형
-#변수와 입력
-#변수 만들기/사용하기
-pi=3.14159265
-print(pi)
-print(pi+2)
-print(pi-2)
-print(pi*2)
-print(pi/2)
-print(pi%2)
-print(pi*pi)
-#원의 둘레와 넓이 구하기
-r=10
-print("원주율=",pi)
-print("반지름=",r)
-print("원의 둘레",2*pi*r)
-print("원의 넓이=",pi*r*r)
+#문자열의 format()함수
 '''
-int pi
-[int]변수 pi를 사용하기 위해서 변수에 저장할 자료형을 선언합니다
+format()함수로 숫자를 문자열로 변환하는 몇 가지 형태를 살펴보겠습니다
 '''
+#format()함수로 숫자를 문자열로 변환하기
+string_a="{}".format(10)
+#출력하기
+print(string_a)
+print(type(string_a))
+
+#format()함수의 다양한 형태
+#format() 함수로 숫자를 문자열로 변환하기
+format_a="{}만 원".format(5000)
+format_b="파이썬 열공하여 첫 연봉 {}만 원 만들기".format(5000)
+format_c="{}{}{}".format(3000,4000,5000)
+format_d="{}{}{}".format(1,"문자열",True)
+#출력하기
+print(format_a)
+print(format_b)
+print(format_c)
+print(format_d)
+#format() 함수의 다양한 기능
+#정수 출력의 다양한 형태
+#정수
+output_a="{:d}".format(52)
+#특정 캉에 출력하기
+output_b="{:5d}".format(52)
+output_c="{:10d}".format(52)
+#빈칸을 0으로 채우기
+output_d="{:05d}".format(52)
+output_e="{:05d}".format(-52)
+
+print("#기본")
+print(output_a)
+print("#특정 칸에 출력하기")
+print(output_b)
+print(output_c)
+print("#빈칸을 0으로 채우기")
+print(output_d)
+print(output_e)
