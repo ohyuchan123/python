9072114175193186daf76c8f57ebf6852e67e6fb
ohyuchan123 <83404310+ohyuchan123@users.noreply.github.com>
2021-07-24 21:29:14 +0900

    Python(조건문-if 조건문)

--- yuchan.python01.py
+++ yuchan.python01.py
@@ -1,41 +1,25 @@
-#자료형
-#문자열의 format()함수
-'''
-format()함수로 숫자를 문자열로 변환하는 몇 가지 형태를 살펴보겠습니다
-'''
-#format()함수로 숫자를 문자열로 변환하기
-string_a="{}".format(10)
-#출력하기
-print(string_a)
-print(type(string_a))
+#조건문
+#if 조건문
+#if 조건문은 조건에 따라 코드를 실행하거나, 실행하지 않게 만들고 싶을 때 사용
+#하는 구문입니다
 
-#format()함수의 다양한 형태
-#format() 함수로 숫자를 문자열로 변환하기
-format_a="{}만 원".format(5000)
-format_b="파이썬 열공하여 첫 연봉 {}만 원 만들기".format(5000)
-format_c="{}{}{}".format(3000,4000,5000)
-format_d="{}{}{}".format(1,"문자열",True)
-#출력하기
-print(format_a)
-print(format_b)
-print(format_c)
-print(format_d)
-#format() 함수의 다양한 기능
-#정수 출력의 다양한 형태
-#정수
-output_a="{:d}".format(52)
-#특정 캉에 출력하기
-output_b="{:5d}".format(52)
-output_c="{:10d}".format(52)
-#빈칸을 0으로 채우기
-output_d="{:05d}".format(52)
-output_e="{:05d}".format(-52)
+if True:
+    print('True입니다..!')
+    print('정말 True입니다..!')
 
-print("#기본")
-print(output_a)
-print("#특정 칸에 출력하기")
-print(output_b)
-print(output_c)
-print("#빈칸을 0으로 채우기")
-print(output_d)
-print(output_e)
+if False:
+    print('False 입니다...!')
+
+#조건문 기본 사용
+number=input("정수 입력>")
+number=int(number)
+
+#양수 조건
+if number>0:
+    print("양수 입니다")
+#음수 조건
+if number<0:
+    print("음수 입니다")
+#0 조건
+if number==0:
+    print("0입니다")
