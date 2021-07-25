6ce09271c10e7a23b075c86a59fc7fa02c1cd19d
ohyuchan123 <83404310+ohyuchan123@users.noreply.github.com>
2021-07-25 21:10:35 +0900

    Python(조건문-else 조건문의 활용)

--- main.py
+++ main.py
@@ -1,27 +1,15 @@
 #조건문
-#날짜/시간 활용하기
-#날짜/시간
-import datetime
+#else 조건문의 활용
+'''
+else 구문은 if 조건문 뒤에 사용하며 if 조건문 뒤에 사용하며,
+if 조건문의 조건이 거짓일 떄 실행되는 부분입니다
+'''
+#짝수와 홀수 구분
+#입력을 받습니다.
+number=int(input("정수 입력>"))
 
-#현재 날짜/시간을 구합니다.
-now=datetime.datetime.now()
-
-#출력합니다.
-print(now.year,"년")
-print(now.month,"월")
-print(now.day,"일")
-print(now.hour,"시")
-print(now.minute,"분")
-print(now.second,"초")
-
-#datetime.datetime.now()라는 함수로 현재의 시간을 구해 now라는 변수에 대입합니다
-
-#출력합니다[방법 2]
-print("{}년 {}월 {}일 {}시 {}분 {}초".format(
-    now.year,
-    now.month,
-    now.day,
-    now.hour,
-    now.minute,
-    now.second
-))
+#조건문을 사용합니다
+if number%2==0:
+    print("짝수 입니다")
+else:
+    print("홀수 입니다")
