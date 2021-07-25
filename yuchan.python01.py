33750b1b84ecef6b38ddc08ac22c068ce7e4f28f
ohyuchan123 <83404310+ohyuchan123@users.noreply.github.com>
2021-07-25 21:10:07 +0900

    Python(조건문-if`~else와 elif 구문)

--- yuchan.python01.py
+++ yuchan.python01.py
@@ -1,25 +1,12 @@
 #조건문
-#if 조건문
-#if 조건문은 조건에 따라 코드를 실행하거나, 실행하지 않게 만들고 싶을 때 사용
-#하는 구문입니다
+#if~else와 elif 구문
 
-if True:
-    print('True입니다..!')
-    print('정말 True입니다..!')
+#입력을 받습니다
+number=int(input("정수 입력>"))
 
-if False:
-    print('False 입니다...!')
-
-#조건문 기본 사용
-number=input("정수 입력>")
-number=int(number)
-
-#양수 조건
-if number>0:
-    print("양수 입니다")
-#음수 조건
-if number<0:
-    print("음수 입니다")
-#0 조건
-if number==0:
-    print("0입니다")
+#짝수 조건
+if number%2==0:
+    print("짝수입니다")
+#홀수 조건
+if number%2==1:
+    print("홀수입니다")
