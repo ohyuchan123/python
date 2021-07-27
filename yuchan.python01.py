e6df8d5e38e13e2c6e30e036ed510ac23022fd75
ohyuchan123 <83404310+ohyuchan123@users.noreply.github.com>
2021-07-27 22:58:21 +0900

    Python(반복문-리스트)

--- yuchan.python01.py
+++ yuchan.python01.py
@@ -1,12 +1,25 @@
-#조건문
-#if~else와 elif 구문
+#반복문
+'''
+리스트 기호인 대괄호[] 안에 들어간 숫자를 인덱스(index)라고 부릅니다.
+'''
 
-#입력을 받습니다
-number=int(input("정수 입력>"))
+list_a=[273,32,103,"문자열",True,False]
+print(list_a[0])
+print(list_a[1])
+print(list_a[2])
+print(list_a[1:3])
 
-#짝수 조건
-if number%2==0:
-    print("짝수입니다")
-#홀수 조건
-if number%2==1:
-    print("홀수입니다")
+list_a[0]="변경"
+print(list_a[0])
+
+#첫째 대괄호 안에 음수를 넣어 뒤에서부터 요소를 선택할 수 있습니다
+print(list_a[-1])
+print(list_a[-2])
+print(list_a[-3])
+#둘째 리스트 접근 연산자를 다음과 같이 이주으로 사용할 수 있습니다
+print(list_a[3])
+print(list_a[3][0])
+#셋째 리스트 안에 리스트를 사용할 수 도 있습니다
+list_b=[[1,2,3],[4,5,6],[7,8,9]]
+print(list_b[1])
+print(list_b[1][1])
