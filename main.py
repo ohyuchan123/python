3fec0523c6964936f6769e25b45d1c5a737b3e7c
ohyuchan123 <83404310+ohyuchan123@users.noreply.github.com>
2021-07-28 23:06:11 +0900

    Python(반복문-리스트에 요소 제거하기)

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
 #반복문
-#리스트 연산자 : 연결(+),반복(*),len()
+#리스트에 요소 제거하기
+#인덱스로 제거하기 : del,pop
+list_a=[0,1,2,3,4,5]
+print("#리스트의 요소 하나 제거하기")
 
-#리스트를 선언합니다
-list_a=[1,2,3]
-list_b=[4,5,6]
-#출력합니다
-print("#리스트")
-print("list_a=",list_a)
-print("list_b=",list_b)
-print()
-#기본 연산자
-print("#리스트 기본 연산자")
-print("list_a+list_b=",list_a+list_b)
-print("list_a*3=",list_a*3)
-print()
-#함수
+#제거 방법[1]-del
+del list_a[1]
+print("del list_a[1] : ",list_a)
+
+#제거 방법[2]-pop
+list_a.pop(2)
+print("pop(2) : ",list_a)
+
+#값으로 제거하기 :remove
 '''
-len() 함수는 괄호 내부에 문자열을 넣으면 문자열의 글자 수(=길이)를 세어 주지만, 리스트 
-변수를 넣으면 요소의 개수를 세어 줍니다. 19행은  list_a에 들어있는 요소의 개수를 구합니다
+리스트.remove(값)
 '''
-print("#길이 구하기")
-print("len(list_a)=",len(list_a))
+list_c=[1,2,1,2]
+list_c.remove(2)
+print(list_c)
+#모두 제거하기 : clear
+'''
+리스트.clear()
+'''
+list_d=[0,1,2,3,4,5]
+list_d.clear()
+print(list_d)
