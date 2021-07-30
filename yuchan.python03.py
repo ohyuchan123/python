bab86c5423b2040b325030daf2a4d0800e132b49
ohyuchan123 <83404310+ohyuchan123@users.noreply.github.com>
2021-07-30 21:30:38 +0900

    Python(반복문-해당하는 값 모두 제거하기)

--- yuchan.python03.py
+++ yuchan.python03.py
@@ -1,11 +1,9 @@
 #반복문
-#for반복문  : 딕셔너리와 함께 사용함
-dictionary={
-    "name":"7D 건조 망고",
-    "type":"당절임",
-    "ingredient":["망고","설탕","메타중아환산나트륨","치자황색소"],
-    "origin":"필리핀"
-}
-#for 반복문을 사용합니다
-for key in dictionary:
-    print(key,":",dictionary[key])
+#해당하는 값 모두 제거하기
+list_test=[1,2,1,2]
+value=2
+
+while value in list_test:
+    list_test.remove(value)
+
+print(list_test)
