3d2688dc1751946e00033da47d12d875e798b457
ohyuchan123 <83404310+ohyuchan123@users.noreply.github.com>
2021-07-30 21:27:57 +0900

    Python(반복문-범위)

--- yuchan.python01.py
+++ yuchan.python01.py
@@ -1,15 +1,16 @@
 #반복문
-#for문
-'''
-for 반복자 in 반복할 수 있는 것 :
-    코드
-'''
-for i in range(100):
-    print("출력")
+#범위
+a=range(5)
+print(a)
+print(list(range(10)))
+print(list(range(0,5))) #0부터 (5-1)까지의 정수로 범위를 만듭니다
+print(list(range(0,10,2))) #0부터 2씩 증가하면서 (10-1_까지의 정수로 범위를 만듭니다
 
-#for 반복문과 리스트
-#리스트를 선언합니다
-array=[273,32,103,57,52]
-#리스트에 반복문을 적용합니다
-for element in array:
-    print(element)
+a=range(10+1)
+print(list(a))
+
+n=10
+a=range(0,int(n/2))#->실수를 정수로 바꾸는 방법보다
+print(list(a))
+a=range(0,n//2)#->정수 나누기 연산자를 많이 사용합니다
+print(list(a))
