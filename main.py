e22d34af17db201ffaae4d5b4d983ff6fd15ccea
ohyuchan123 <83404310+ohyuchan123@users.noreply.github.com>
2021-07-30 21:28:44 +0900

    Python(반복문-for 반복문 : 범위와 함께 사용하기)

--- main.py
+++ main.py
@@ -1,26 +1,15 @@
 #반복문
-#딕셔너리와 반복문
-#딕셔너리 선언하기
+#for 반복문 : 범위와 함께 사용하기
 '''
-딕셔너리는 중괄호{}로 선언하며, '키:값' 형태를 쉼표(,)로 연결해서 만듭니다. 키는 문자열,숫자,불 등으로 선언할 수 있습니다
-변수={
-    키:값
-    키:값
-    ...
-    키:값
+for 숫자 변수 in 범위:
+    코드
 '''
-dict_a={
-    "name":"어밴져스 엔드게임",
-    "type":"히어로 무비"
-}
-
-print(dict_a)
-print(dict_a["name"])
-print(dict_a["type"])
-
-dict_b={
-    "director":["안소니 루소","조 루소"],
-    "cast":["아이언맨","타노스","토르","닥터스트레인지","헐크"]
-}
-print(dict_b)
-print(dict_b["director"])
+for i in range(5):
+    print(str(i)+"=반복 변수")
+print()
+for i in range(5,10):
+    print(str(i)+"=반복 변수")
+print()
+for i in range(0,10,3):
+    print(str(i)+"=반복 변수")
+print()
