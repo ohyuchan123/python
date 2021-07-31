39c195fa13ce765ff25b913da03c51109540f218
ohyuchan123 <83404310+ohyuchan123@users.noreply.github.com>
2021-07-31 22:20:44 +0900

    Python(반복문-enumerate() 함수와 반복문 조합하기)

--- yuchan.python03.py
+++ yuchan.python03.py
@@ -1,9 +1,30 @@
 #반복문
-#해당하는 값 모두 제거하기
-list_test=[1,2,1,2]
-value=2
+#enumerate() 함수와 반복문 조합하기
+'''
+example_list=["요소A","요소B","요소C"]
+'''
+#방법(1)
+'''
+example_list=["요소A","요소B","요소C"]
+i=0
+for item in example_list:
+    print("{}번째 요소는{}입니다.".format(i,item))
+    i+=1
+'''
+#방법(2)
+'''
+example_list=["요소A","요소B","요소C"]
+for i in range(len(example_list)):
+    print("{}번째 요소는 {}입니다".format(i,example_list[i])
+'''
+example_list=["요소A","요소B","요소C"]
+print("#단순 출력")
+print(example_list)
+print()
 
-while value in list_test:
-    list_test.remove(value)
+print("#enumerate() 함수 적용 출력")
+print(enumerate(example_list))
+print()
 
-print(list_test)
+print("#list() 함수로 강제 변환 출력")
+print(list(enumerate(example_list)))
