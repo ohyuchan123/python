d047812008c155107dacd8dbb26a35c9a5ef73f4
ohyuchan123 <83404310+ohyuchan123@users.noreply.github.com>
2021-07-31 22:18:35 +0900

    Python(반복문-while : break 키워드/continue 키워드)

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 #반복문
-#for 반복문 : 범위와 함께 사용하기
-'''
-for 숫자 변수 in 범위:
-    코드
-'''
-for i in range(5):
-    print(str(i)+"=반복 변수")
-print()
-for i in range(5,10):
-    print(str(i)+"=반복 변수")
-print()
-for i in range(0,10,3):
-    print(str(i)+"=반복 변수")
-print()
+#while 반복문 : break 키워드/continue 키워드
+i=0
+while True:
+    print("{}번째 반복문입니다.".format(i))
+    i+=1
+    input_text=input(">종료하시겠습니까?(y/n) : ")
+    if input_text in["y","Y"]:
+        print("반복을 종료합니다.")
+        break
+    elif input_text in["n","N"]:
+        print("반복을 계속합니다")
+
+#continue 키워드
+numbers=[5,15,6,20,7,25]
+
+for number in numbers:
+    if number<10:
+        continue
+    print(number)
