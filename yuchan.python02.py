1a115f56258a16cb8816aa11e3f44c4cb272f6a3
ohyuchan123 <83404310+ohyuchan123@users.noreply.github.com>
2021-08-01 20:18:12 +0900

    Python(반복문-별찍기)

--- yuchan.python02.py
+++ yuchan.python02.py
@@ -1,11 +1,72 @@
 #반복문
-#리스트에 적용할 수 있는 기본 함수 : min(),max(),sum()
+#별찍기
 '''
-min() : 리스트 내부에서 최솟값을 찾습니다
-max() : 리스트 내부에서 최댓값을 찾습니다
-sum() : 리스트 내부에서 값을 모두 더합니다
+*
+**
+***
+****
+*****
 '''
-number=[103,52,273,32,77]
-print("최솟값 : ",min(number))
-print("최댓값 : ",max(number))
-print("합 : ",sum(number))
+#간단한 코드
+for i in range(5):
+    print('{:<5}'.format('*'*(i+1)))
+#일반코드
+for i in range(5):
+    for j in range(i+1):
+        print('*',end="")
+    print('')
+print()
+'''
+*****
+****
+***
+**
+*
+'''
+#간단한 코드
+for i in range(6,0,-1):
+    print('{:<5}'.format('*'*(i-1)))
+#일반 코드
+for i in range(5,0,-1):
+    for j in range(i):
+        print('*',end="")
+    print('')
+print()
+
+'''
+*****
+ ****
+  ***
+   **
+    *
+'''
+#간단한 코드
+for i in range(6,0,-1):
+    print('{:>5}'.format('*'*(i-1)))
+#일반코드
+for i in range(5):
+    for j in range(i):
+        print(' ',end="")
+    for j in range(5-i):
+        print('*',end="")
+    print(' ')
+print()
+
+'''
+    *
+   **
+  ***
+ ****
+*****
+'''
+#간단한 코드
+for i in range(5):
+    print('{:>5}'.format('*'*(i+1)))
+#일반코드
+for i in range(1,6):
+    for j in range(5-i):
+        print(' ',end="")
+    for j in range(i):
+        print('*',end="")
+    print('')
+print()
