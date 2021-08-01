a4ed02fc8094219ac15d8afa8a6424b0c9539497
ohyuchan123 <83404310+ohyuchan123@users.noreply.github.com>
2021-08-02 08:42:37 +0900

    Python(함수-기본적인 함수의 활용)

--- yuchan.python03.py
+++ yuchan.python03.py
@@ -1,66 +1,32 @@
-#반복문
-#별찍기
+#함수
+#기본적인 함수의 활용
 '''
-    *
-   ***
-  *****
- *******
-*********
-*********
- *******
-  *****
-   ***
-    *
+def 함수(매개변수):
+    변수=초깃값
+    #여러 가지 처리
+    #여러 가지 처리
+    #여러 가지 처리
+    return 변수
 '''
-#간단한 코드
-for i in range(1, 11, 2):
-    print('{:^10}'.format('*' * i))
-for i in range(9, 0, -2):
-    print('{:^10}'.format('*' * i))
-print()
-#일반 코드
-for i in range(1,6):
-    for j in range(5-i):
-        print(' ',end="")
-    for j in range(1,i*2,1):
-        print('*',end="")
-    print('')
-for i in range(5):
-    for j in range(i):
-         print(' ', end="")
-    for j in range(10, 1 + i * 2, -1):
-        print('*', end="")
-    print('')
-print()
-'''
-*********
- *******
-  *****
-   ***
-    *
-    *
-   ***
-  *****
- *******
-*********
-'''
-#간단한 코드
-for i in range(9, 0, -2):
-    print('{:^10}'.format('*' * i))
-for i in range(3, 11, 2):
-    print('{:^10}'.format('*' * i))
-print()
-#일반 코드
-for i in range(5):
-    for j in range(i):
-         print(' ', end="")
-    for j in range(10, 1 + i * 2, -1):
-        print('*', end="")
-    print('')
-for i in range(1,6):
-    for j in range(5-i):
-        print(' ',end="")
-    for j in range(1,i*2,1):
-        print('*',end="")
-    print('')
-print()
+#범위 내부의 정수를 모두 더하는 함수
+def sum_all(start,end):
+    output=0
+    for i in range(start,end+1):
+        output+=i
+    return output
+
+print("0 to 100 : ",sum_all(0,100))
+print("0 to 1000 : ",sum_all(0,1000))
+print("50 to 100 : ",sum_all(50,100))
+print("500 to 1000 : ",sum_all(500,1000))
+
+#기본 매개변수와 키워드 매개변수를 활용해 범위의 정수를 더하는 함수
+def sum_all(start=0,end=100,step=1):
+    output=0
+    for i in range(start,end+1,step):
+        output+=i
+    return output
+
+print("A.",sum_all(0,100,10))
+print("B.",sum_all(end=100))
+print("C.",sum_all(end=100,step=2))
