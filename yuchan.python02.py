08f06332b8644ac60d3c2e4419c47e59e1f82989
ohyuchan123 <83404310+ohyuchan123@users.noreply.github.com>
2021-08-02 08:40:47 +0900

    Python(함수-키워드 매개 변수 및 함수의 여러 형태)

--- yuchan.python02.py
+++ yuchan.python02.py
@@ -1,72 +1,26 @@
-#반복문
-#별찍기
+#함수
+#키워드 매개 변수
 '''
-*
-**
-***
-****
-*****
+print(value,...,sep=' ',end='\n',file=sys.stdout,flush=False)
+이름을 지정해서 입력하는 매개변수를 키워드 매개변수라고 부릅니다
 '''
-#간단한 코드
-for i in range(5):
-    print('{:<5}'.format('*'*(i+1)))
-#일반코드
-for i in range(5):
-    for j in range(i+1):
-        print('*',end="")
-    print('')
-print()
-'''
-*****
-****
-***
-**
-*
-'''
-#간단한 코드
-for i in range(6,0,-1):
-    print('{:<5}'.format('*'*(i-1)))
-#일반 코드
-for i in range(5,0,-1):
-    for j in range(i):
-        print('*',end="")
-    print('')
-print()
+def print_n_times(*values,n=2):
+    for i in range(n):
+        for value in values:
+            print(value)
+        print()
 
-'''
-*****
- ****
-  ***
-   **
-    *
-'''
-#간단한 코드
-for i in range(6,0,-1):
-    print('{:>5}'.format('*'*(i-1)))
-#일반코드
-for i in range(5):
-    for j in range(i):
-        print(' ',end="")
-    for j in range(5-i):
-        print('*',end="")
-    print(' ')
-print()
+#함수 호출
+print_n_times("안녕하세요","즐거운","파이썬 프로그래밍",n=3)
 
-'''
-    *
-   **
-  ***
- ****
-*****
-'''
-#간단한 코드
-for i in range(5):
-    print('{:>5}'.format('*'*(i+1)))
-#일반코드
-for i in range(1,6):
-    for j in range(5-i):
-        print(' ',end="")
-    for j in range(i):
-        print('*',end="")
-    print('')
-print()
+#열 함수 호출 형태
+def test(a,b=10,c=100):
+    print(a+b+c)
+#1)기본 형태
+test(10,20,30)
+#2)키워드 매개변수로 모든 매개변수를 지정한 형태
+test(a=10,b=100,c=200)
+#3)키워드 매개변수로 모든 매개변수를 마구잡이로 지정한 형태
+test(c=10,a=100,b=200)
+#4)키워드 매개변수로 일부 매개변수만 지정한 형태
+test(10,c=200)
