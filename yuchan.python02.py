e523f89ff072be176a0fa6b614c0cc3cfad09162
ohyuchan123 <83404310+ohyuchan123@users.noreply.github.com>
2021-08-04 07:37:28 +0900

    Python(함수-filter()함수와 map()함수)

--- yuchan.python02.py
+++ yuchan.python02.py
@@ -1,33 +1,26 @@
 #함수
-#재귀 함수로 구현한 피보나치 수열(1)
-def fibonacci(n):
-    if n==1:
-        return 1
-    if n==2:
-        return 1
-    else :
-        return fibonacci(n-1)+fibonacci(n-2)
-#함수 호출
-print("1! : ",fibonacci(1))
-print("2! : ",fibonacci(2))
-print("3! : ",fibonacci(3))
-print("4! : ",fibonacci(4))
-print("5! : ",fibonacci(5))
+#filter()함수와 map()함수
+'''
+함수를 매개변수로 전달하는 대표적인 표준함수로 map() 함수와 filter()함수가 있다
+map() : map(함수,리스트)
+filter() : filter(함수,리스트)
+'''
+def power(item):
+    return item*item
+def under_3(item):
+    return item<3
 
-#재귀 함수로 구현한 피보나치 수열(2)
-counter=0
-def fibonacci(n):
-    print("fibonacci({})를 구합니다.".format(n))
-    global counter
-    counter+=1
-    if n==1:
-        return 1
-    if n==2:
-        return 1
-    else:
-        return fibonacci(n-1)+fibonacci(n-2)
+list_input_a=[1,2,3,4,5]
 
-#함수를 호출합니다
-fibonacci(10)
-print("---")
-print("fibonacci(10) 계산에 활ㅇㅇ된 덧셈의 횟수는 {}입니다.".format(counter))
+#map() 함수 사용
+output_a=map(power,list_input_a)
+print("# map() 함수의 실행결과")
+print("map(power,list_input_a) : ",output_a)
+print("map(power,list_input_a) : ",list(output_a))
+
+#filter() 함수 사용
+output_b=filter(under_3,list_input_a)
+print("# filter() 함수의 실행결과")
+print("filter(under_3,list_input_a) : ",output_b)
+print("filter(under_3,list_input_a) : ",list(output_b))
+
