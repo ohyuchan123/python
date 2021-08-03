9bf4c3fbc6164d83e8a3db96bc0c980e967b5743
ohyuchan123 <83404310+ohyuchan123@users.noreply.github.com>
2021-08-03 10:10:20 +0900

    Python(함수-재귀 함수로 구현한 피보나치 수열)

--- yuchan.python02.py
+++ yuchan.python02.py
@@ -1,26 +1,33 @@
 #함수
-#키워드 매개 변수
-'''
-print(value,...,sep=' ',end='\n',file=sys.stdout,flush=False)
-이름을 지정해서 입력하는 매개변수를 키워드 매개변수라고 부릅니다
-'''
-def print_n_times(*values,n=2):
-    for i in range(n):
-        for value in values:
-            print(value)
-        print()
-
+#재귀 함수로 구현한 피보나치 수열(1)
+def fibonacci(n):
+    if n==1:
+        return 1
+    if n==2:
+        return 1
+    else :
+        return fibonacci(n-1)+fibonacci(n-2)
 #함수 호출
-print_n_times("안녕하세요","즐거운","파이썬 프로그래밍",n=3)
+print("1! : ",fibonacci(1))
+print("2! : ",fibonacci(2))
+print("3! : ",fibonacci(3))
+print("4! : ",fibonacci(4))
+print("5! : ",fibonacci(5))
 
-#열 함수 호출 형태
-def test(a,b=10,c=100):
-    print(a+b+c)
-#1)기본 형태
-test(10,20,30)
-#2)키워드 매개변수로 모든 매개변수를 지정한 형태
-test(a=10,b=100,c=200)
-#3)키워드 매개변수로 모든 매개변수를 마구잡이로 지정한 형태
-test(c=10,a=100,b=200)
-#4)키워드 매개변수로 일부 매개변수만 지정한 형태
-test(10,c=200)
+#재귀 함수로 구현한 피보나치 수열(2)
+counter=0
+def fibonacci(n):
+    print("fibonacci({})를 구합니다.".format(n))
+    global counter
+    counter+=1
+    if n==1:
+        return 1
+    if n==2:
+        return 1
+    else:
+        return fibonacci(n-1)+fibonacci(n-2)
+
+#함수를 호출합니다
+fibonacci(10)
+print("---")
+print("fibonacci(10) 계산에 활ㅇㅇ된 덧셈의 횟수는 {}입니다.".format(counter))
