1fb1e570ef609b517d11f63629976fbbccfc40f0
ohyuchan123 <83404310+ohyuchan123@users.noreply.github.com>
2021-08-04 07:36:33 +0900

    Python(함수-변수의 값을 교환하는 튜플)

--- yuchan.python01.py
+++ yuchan.python01.py
@@ -1,22 +1,25 @@
 #함수
-#재귀 함수
-'''
-팩토리얼(factorial)
-n!=n*(n-1)*(n-2)*...*1
-'''
-#반복문으로 팩토리얼 구하기
-def factorial(n):
-    output=1
-    for i in range(1,n+1):
-        output*=i
-    return output
+#변수의 값을 교환하는 튜플
+a,b=10,20
+print("#교환 전 값")
+print("a : ",a)
+print("b : ",b)
+print()
 
-#함수 호출
-print("1! : ",factorial(1))
-print("2! : ",factorial(2))
-print("3! : ",factorial(3))
-print("4! : ",factorial(4))
-print("5! : ",factorial(5))
+#값을 교환합니다
+a,b=b,a
 
+print("#교환 후 값")
+print("a : ",a)
+print("b : ",b)
+print()
+
+#튜플 과 함수
+def test():
+    return (10,20)
+a,b=test()
+
+print("a : ",a)
+print("b : ",b)
 
 
