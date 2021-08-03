15aa0b91d89aeec6fb551a972e8274328ab0eea3
ohyuchan123 <83404310+ohyuchan123@users.noreply.github.com>
2021-08-03 10:09:49 +0900

    Python(함수-재귀 함수를 사용해 팩토리얼 구하기)

--- main.py
+++ main.py
@@ -1,28 +1,14 @@
 #함수
-#가변 매개변수
-'''
-def 함수이름(매개변수,매개변수,...,*가변 매개변수):
-    문장
-'''
-
-#가변 매개변수 함수
-def print_n_times(n,*values):
-    for i in range(n):
-        for value in values:
-            print(value)
-        print()
+#재귀 함수를 사용해 팩토리얼 구하기
+def factorial(n):
+    if n==0:
+        return 1
+    else:
+        return n*factorial(n-1)
 
 #함수 호출
-print_n_times(3,"안녕하세요","즐거운","파이썬 프로그래밍")
-
-#기본 매개변수
-'''
-print(value,...,sep=' ',end='\n',file=sys.stdout,flush=False)
-'''
-#기본 매개변수
-def print_n_times(value,n=2):
-    for i in range(n):
-        print(value)
-
-#함수 호출
-print_n_times("안녕하세요")
+print("1! : ",factorial(1))
+print("2! : ",factorial(2))
+print("3! : ",factorial(3))
+print("4! : ",factorial(4))
+print("5! : ",factorial(5))
