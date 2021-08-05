9e777abed32133480ae1fa6bc8e7fb5a0535951a
ohyuchan123 <83404310+ohyuchan123@users.noreply.github.com>
2021-08-05 17:59:27 +0900

    Python(함수-with 키워드)

--- yuchan.python01.py
+++ yuchan.python01.py
@@ -1,25 +1,23 @@
 #함수
-#변수의 값을 교환하는 튜플
-a,b=10,20
-print("#교환 전 값")
-print("a : ",a)
-print("b : ",b)
-print()
+#with 키워드
+'''
+프로그램이 킬어지면  open() 함수와 close() 함수 사이에 많은 코드가 들어갑니다 조건문과 반복
+문이 들어가다 보면 파일을 열고 닫지 않는 실수를 한는 경우가 생길 수 있습니다 이런 실수를 방지하기 위해
+with 키워드 라는 기능이 생겼습니다
+with open(문자열 : 파일 경로,문자열 : 모드) as 파일 객체:
+    문장
+'''
+with open("basic.txt","w") as file:
+    file.write("Helllo Python Programming...!")
 
-#값을 교환합니다
-a,b=b,a
-
-print("#교환 후 값")
-print("a : ",a)
-print("b : ",b)
-print()
-
-#튜플 과 함수
-def test():
-    return (10,20)
-a,b=test()
-
-print("a : ",a)
-print("b : ",b)
+#텍스트 읽기
+'''
+파일에 텍스트를 쓸 때는 방금 살펴보았던 것처럼  write() 함수를 사용합니다.
+반대로 파일을 읽을 때는 read() 함수를 사용합니다
+파일 객체.read()
+'''
+with open("basic.txt","r") as file:
+    contents=file.read()
+print(contents)
 
 
