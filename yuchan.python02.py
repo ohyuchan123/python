c2798a4a78a2c10923a4500ecf8fcf46ef91df3a
ohyuchan123 <83404310+ohyuchan123@users.noreply.github.com>
2021-08-05 18:01:44 +0900

    Python(함수-제네레이터)

--- yuchan.python02.py
+++ yuchan.python02.py
@@ -1,26 +1,12 @@
 #함수
-#filter()함수와 map()함수
-'''
-함수를 매개변수로 전달하는 대표적인 표준함수로 map() 함수와 filter()함수가 있다
-map() : map(함수,리스트)
-filter() : filter(함수,리스트)
-'''
-def power(item):
-    return item*item
-def under_3(item):
-    return item<3
+#제네레이터
+def test():
+    print("함수가 호출되었습니다.")
+    yield "test"
 
-list_input_a=[1,2,3,4,5]
+print("A 지점 통과")
+test()
 
-#map() 함수 사용
-output_a=map(power,list_input_a)
-print("# map() 함수의 실행결과")
-print("map(power,list_input_a) : ",output_a)
-print("map(power,list_input_a) : ",list(output_a))
-
-#filter() 함수 사용
-output_b=filter(under_3,list_input_a)
-print("# filter() 함수의 실행결과")
-print("filter(under_3,list_input_a) : ",output_b)
-print("filter(under_3,list_input_a) : ",list(output_b))
-
+print("B 지점 통과")
+test()
+print(test())
