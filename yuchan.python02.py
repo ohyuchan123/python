8146c938213a2d3a9708b77338d5faeee77c67ea
ohyuchan123 <83404310+ohyuchan123@users.noreply.github.com>
2021-08-06 20:58:41 +0900

    Python(모듈-as 구문)

--- yuchan.python02.py
+++ yuchan.python02.py
@@ -1,12 +1,12 @@
-#함수
-#제네레이터
-def test():
-    print("함수가 호출되었습니다.")
-    yield "test"
-
-print("A 지점 통과")
-test()
-
-print("B 지점 통과")
-test()
-print(test())
+#모듈
+#as 구문
+'''
+추가로 모듈의 이름이 너무 길어서 짧게 줄여 사용하고 싶은 경우 as 구문을 사용한다
+import 모듈 as 사용하고 싶은 식별자
+'''
+import math as m
+print(m.sin(1))
+print(m.cos(1))
+print(m.tan(1))
+print(m.floor(2.5))
+print(m.floor(2.5))
