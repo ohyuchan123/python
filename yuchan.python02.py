099d2a5b20b7ccfe1aaabba78f2de1416886fc15
ohyuchan123 <83404310+ohyuchan123@users.noreply.github.com>
2021-08-07 20:03:24 +0900

    Python(모듈-시간 처리하기)

--- yuchan.python02.py
+++ yuchan.python02.py
@@ -1,12 +1,19 @@
 #모듈
-#as 구문
-'''
-추가로 모듈의 이름이 너무 길어서 짧게 줄여 사용하고 싶은 경우 as 구문을 사용한다
-import 모듈 as 사용하고 싶은 식별자
-'''
-import math as m
-print(m.sin(1))
-print(m.cos(1))
-print(m.tan(1))
-print(m.floor(2.5))
-print(m.floor(2.5))
+#시간 처리하기
+import datetime
+now=datetime.datetime.now()
+
+print("# datetime.timedelta로 시간 더하기")
+after=now+datetime.timedelta(
+    weeks=1,\
+    days=1,\
+    hours=1,\
+    minutes=1,\
+    seconds=1,
+)
+print(after.strftime("%Y{} %m{} %d{} %H{} %M{} %S{}").format(*"년월일시분초"))
+print()
+
+print("# now.replace()로 1년 더하기")
+output=now.replace(year=(now.year+1))
+print(output.strftime(("%Y{} %m{} %d{} %H{} %M{} %S{}").format(*"년월일시분초")))
