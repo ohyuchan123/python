2ace9661a9f036e18732e838cdc448beecd6f521
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2021-09-08 20:07:25 +0900

    Python(복습-숫자 처리 함수)

---
+++ Number processing function.py
@@ -0,0 +1,21 @@
+#숫자 처리 함수
+
+#절대값
+print(abs(-5)) # 5
+#제곱
+print(pow(4,2)) # 4^2=4*4=16
+#최대값
+print(max(5,12)) # 12
+#최솟값
+print(min(5,12)) # 5
+#반올림
+print(round(3.14)) # 3
+print(round(4.99)) # 5
+
+from math import*
+#내림
+print(floor(4.99))
+#올림
+print(ceil(3.14))
+#제곱근
+print(sqrt(16))
