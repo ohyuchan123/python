506a9cfd41165fb1f50f979855e4e92f16895fc8
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2021-09-08 20:07:44 +0900

    Python(복습-랜덤함수)

---
+++ Random function.py
@@ -0,0 +1,19 @@
+from random import*
+
+# print(random()) #0.0 ~ 1.0 미만의 임이의 값 생성
+# print(random()*10) #0.0 ~10.0 미만의 임이의 값 생성
+# print(int(random()*10)) # 0부터 10미만의 임이의 값 생성
+# print(int(random()*10+1)) #1부터 10이하의 임이의 값 생성
+
+print(int(random()*45+1)) #1~45 이하의 임이의 값 생성
+print(int(random()*45+1)) #1~45 이하의 임이의 값 생성
+print(int(random()*45+1)) #1~45 이하의 임이의 값 생성
+print(int(random()*45+1)) #1~45 이하의 임이의 값 생성
+print(int(random()*45+1)) #1~45 이하의 임이의 값 생성
+print(int(random()*45+1)) #1~45 이하의 임이의 값 생성
+print(int(random()*45+1)) #1~45 이하의 임이의 값 생성
+
+print(randrange(1,46)) #1~46 미만의 임이의 값 생성
+
+print(randint(1,45)) #1~45 이하의 임이의 값 생성
+
