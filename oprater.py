bcf0c2f8ac0c204a26c67fc8fbabf25a0f879bf1
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2021-09-08 20:06:19 +0900

    Python(복습-연산자)

---
+++ oprater.py
@@ -0,0 +1,34 @@
+print(1+1) # 2
+print(3-2) # 1
+print(5*2) # 10
+print(6/2) # 3
+
+print(2**3) # 2^3=8
+print(5%3) # 나머지 구하기 2
+print(10%3) # 1
+print(5//3) # 1
+print(10//3) # 3
+
+print(10>3) # True
+print(4>=7) # False
+print(10<3) # False
+print(5<=5) # True
+
+print(3==3) # True
+print(4==3) # False
+print(3+4==7) #True
+
+#not
+print(1!=3) # True
+print(not(1!=3)) #False
+
+#and
+print((3>0) and (3<5)) # True
+print((3>0) & (3<5)) # True
+
+#or
+print((3>0) or (3>5)) # True
+print((3>0)|(3>5)) # True
+
+print(5>4>3) # True
+print(5>4>7) # False
