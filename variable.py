c96fc50236538d35a2a426361b8537aac8eaf9e1
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2021-09-08 20:05:48 +0900

    Python(복습-변수)

---
+++ variable.py
@@ -0,0 +1,14 @@
+#변수 : 값을 저장하는 공간
+#애완동물을 소개해 주세요
+
+animal='강아지'
+name='연탄'
+age=4
+hobby='산책'
+is_adult=age>=3
+
+
+
+print("우리집 "+animal+"의 이름은 "+name+"이에요")
+print(name+"는"+str(age)+"살이며,"+hobby+"을 아주 좋아해요")
+print(name+"는 어린일까요 ?"+str(is_adult))
