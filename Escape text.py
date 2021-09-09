479994a7a8edd98f3c458c5e3699062a93dce8a7
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2021-09-09 17:58:21 +0900

    Python(복습-탈출문자)

---
+++ Escape text.py
@@ -0,0 +1,16 @@
+# \n : 줄바꿈
+print("백문이 불여일견\n백견이 불여일타")
+
+# \" \' : 문장 내에서 따옴표 
+#저는 "나도코딩" 입니다
+print('저는 "나도코딩"입니다')
+print("저는 \"나도코딩\"입니다")
+
+# \\ : 문장내에서는 \
+print("C:\\Users\\user\\Desktop\\Python공부>")
+
+# \r : 커서를 맨 앞으로 이동
+print("Red Apple\rPine")
+
+# \b : 백스페이스(한 글자 삭제)
+print("Redd\bApple")
