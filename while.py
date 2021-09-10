09bc96bffa3062669c26945c283baad04b9d8457
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2021-09-10 22:33:10 +0900

    Python(복습-while문)

---
+++ while.py
@@ -0,0 +1,14 @@
+#while
+customer="토르"
+index=5
+#조건이 만족 할 때 까지 반복함
+while index>=1:
+    print("{0}, 커피가 준비 되었습니다. {1}번 남았어요.".format(customer,index))
+    index-=1
+
+#무한 루프
+# customer="아이언맨"
+# index=1
+# while True:
+#     print("{0}, 커피가 준비 되었습니다. {1}번 남았어요.".format(customer,index))
+#     index+=1
