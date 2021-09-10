036c805059ccd9bdcaeb2e2e30e3632a39d01c22
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2021-09-10 22:16:48 +0900

    Python(복습-튜플)

---
+++ tuple.py
@@ -0,0 +1,13 @@
+menu=("돈까스","치즈까스")
+print(menu[0])
+print(menu[1])
+
+#튜플은 값을 추가하거나 변경할 수가 없다
+
+name="김종국"
+age=45
+hobby='운동'
+print(name,age,hobby)
+
+(name,age,hobby)=("유재석","49","신문읽기")
+print(name,age,hobby)
