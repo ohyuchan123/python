80a52be821fbe8a51fdf24ce3ac752514f3b83b6
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2021-09-11 21:13:58 +0900

    Python(복습-가변인자)

---
+++ VariadicArguments.py
@@ -0,0 +1,7 @@
+def profile(name,age,lang1,lang2,lang3,lang4,lang5):
+    print("이름 : {0}\t나이 : {1}\t".format(name,age),end=" ")
+    print(lang1,lang2,lang3,lang4,lang5)
+
+
+profile("오유찬",17,"Python","Java","Node.js","C","HTML")
+profile("손정민",17,"Java","C","HTML","","")
