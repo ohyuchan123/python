f035de89ee9b692c7b70183dc8b582c6e1b491c5
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2021-09-11 21:14:39 +0900

    Python(복습-지역변수와 전역변수)

---
+++ RegionalvariableGlobalvariable.py
@@ -0,0 +1,15 @@
+gun=10
+
+def checkpoint(soldiers):
+    global gun #전역 공간에 있는 gun 사용 
+    gun-=soldiers
+    print("[함수 내] 남은 총 : {0}".format(gun))
+def checkpoint_ret(gun,soldiers):
+    gun-=soldiers
+    print("[함수 내] 남은 총 : {0}".format(gun))
+    return gun
+
+print("전체 총 : {0}".format(gun))
+checkpoint(2)
+gun=checkpoint_ret(gun,2)
+print("남은 총 : {0}".format(gun))
